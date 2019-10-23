a138e2aa9aa75d988c995c5d793d001a64786540
unknown <officialgopisingh@gmail.com>
2019-10-23 13:44:20 +0530

    start Logo

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+const Container = styled.div`
+  .test {
+    background-color: black;
+    padding-left: 70px;
+    padding-right: 40px;
+  }
+`
+
+export default () => (
+  <Container>
+    <p>hi</p>
+  </Container>
+)
